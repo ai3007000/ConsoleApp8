e70dddbfc0fe254c33a2adbc2775da50c4b7fa2a
Артём Иванов <ai5007000@yandex.ru>
2023-12-07 20:37:56 +0700

    Решение первого задания

---
+++ ConsoleApp3/MyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// Моё исключение
+    /// </summary>
+    class MyException : Exception
+    {
+        public MyException() { }
+        public MyException(string message) : base(message) { }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace ConsoleApp3.Task
+namespace ConsoleApp3.Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                ICalculate calculate = new Calculate();
 
+                Console.WriteLine("Введите первое слогаемое.");
+                if (!double.TryParse(Console.ReadLine(), out var a)) throw new MyException("Невернный ввод первого слогаемого.");
+
+                Console.WriteLine("Введите второе слогаемое.");
+                if (!double.TryParse(Console.ReadLine(), out var b)) throw new MyException("Неверный ввод второго слогаемого.");
+
+                Console.WriteLine($"Результат: {calculate.OperationPlus(a, b)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
